9b7a9155314977b1ba9a7e06b8682a863beb6877
piushanCnakandala <piushancnakandala2000@gmail.com>
2022-09-12 11:31:31 +0530

    updtate

--- src/pages/post/index.jsx
+++ src/pages/post/index.jsx
@@ -3,6 +3,7 @@ import {styleSheet} from "./style";
 import {withStyles} from "@mui/styles";
 import {Grid, TextField, Typography} from "@mui/material";
 import StyleButton from "../../components/common/Button";
+import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 
 class Post extends Component{
     constructor(props) {
@@ -13,27 +14,34 @@ class Post extends Component{
         return(
             <Fragment>
                 <Typography variant="h4">Post Manager</Typography>
+
+                <ValidatorForm
+                    ref="form"
+                    onSubmit={this.handleSubmit}
+                    onError={errors => console.log(errors)}
+                >
+
                 <Grid className={classes.main_container} container spacing={0.5}>
                     <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField className={classes.txt_style} label={"User id"} variant={"outlined"} size={"small"}/>
+                        <TextValidator className={classes.txt_style} label={"User id"} variant={"outlined"} size={"small"}/>
                     </Grid>
 
                     <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField className={classes.txt_style} label={" Id"} variant={"outlined"} size={"small"}/>
+                        <TextValidator className={classes.txt_style} label={" Id"} variant={"outlined"} size={"small"}/>
                     </Grid>
 
                     <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField  className={classes.txt_style}label={"Title"} variant={"outlined"} size={"small"}/>
+                        <TextValidator  className={classes.txt_style}label={"Title"} variant={"outlined"} size={"small"}/>
                     </Grid>
 
 
                     <Grid item lg={6} md={6} sm={6} xm={6}>
-                        <TextField className={classes.txt_style} label={" Body"} variant={"outlined"} size={"small"}/>
+                        <TextValidator className={classes.txt_style} label={" Body"} variant={"outlined"} size={"small"}/>
                     </Grid>
                     <StyleButton variant="contained" label="Save"/>
                 </Grid>
 
-
+                    <ValidatorForm/>
             </Fragment>
         )
     }
